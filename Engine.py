134adaeb322b8fbd645433b650bef04e97e74c68
Marlowe <marlowe1999@hotmail.co.uk>
2021-03-21 03:25:10 +0000

    Added blocking and refactored moves
    -Returned different colour for empty square
    - Added function that takes direction name and outputs corresponding move
    -Added BEEFY function for finding moves in any direction, colour of piece and whether it is blocked or can be taken
    -Refactored pawn, bishop, rook and queen moves with BEEFY function
    -Added TODO's

--- Engine.py
+++ Engine.py
@@ -2,11 +2,11 @@
 # To add a new markdown cell, type '# %% [markdown]'
 # %%
 board = [
-    ['♜', '♞', '♝', '♛', '♚', '♝', '♞', '♜'],
+    [' ', '♞', '♝', '♛', '♚', '♝', '♞', '♜'],
     ['♟', '♟', '♟', '♟', '♟', '♟', '♟', '♟'],
     [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
     [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', '♜', ' ', ' ', ' '],
     [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
     ['♙', '♙', '♙', '♙', '♙', '♙', '♙', '♙'],
     ['♖', '♘', '♗', '♕', '♔', '♗', '♘', '♖']
@@ -53,76 +53,97 @@ def colour_of_piece(piece):
         return 'white'
     elif piece == '♔':
         return 'white'
+    elif piece == ' ':
+        return 'empty'
     else:
         return 'black'
 
 
 # %%
+def direction_move(direction):
+    if direction == 'north':
+        return 0, -1
+    if direction == 'north-east':
+        return 1, -1
+    if direction == 'east':
+        return 1, 0
+    if direction == 'south-east':
+        return 1, 1
+    if direction == 'south':
+        return 0, 1
+    if direction == 'south-west':
+        return -1, 1
+    if direction == 'west':
+        return -1, 0
+    if direction == 'north-west':
+        return -1, -1
+    
 
 
+# %%
+def find_moves_in_direction(x_pos, y_pos, our_colour, direction, max_moves):
+    x_off, y_off = direction_move(direction)
+    x_target = x_pos
+    y_target = y_pos
+    for moves in range(max_moves):
+        x_target += x_off
+        y_target += y_off
+        if not moves_on_board(x_target, y_target):
+            break
+        target = board[y_target][x_target]
+        target_colour = colour_of_piece(target)
+        if our_colour == target_colour:
+            print('Blocked by', target+'. Now ending search in', direction+ 'ward direction')
+            break
+        print('Considering North-Westward move to x:', x_target, 'and y:', y_target)
+        if our_colour != target_colour and target_colour != 'empty': 
+            print('Taking', target+'. Now ending search in', direction+'ward direction')
+            break
+
+
+# %%
 def find_king_moves(x, y):
+    # TODO use find moves in direction function to check for blocking and capture
     for row_offset in range(y - 1, y + 2):
         for col_offset in range(x - 1, x + 2):
             if row_offset != y or col_offset != x:
                 if moves_on_board(col_offset, row_offset):
                     print('Considering move to x:', col_offset, 'and y:', row_offset)
+                
     print()
 
 
-def find_pawn_moves(x, y):
-    if y == 6:
-        print('Considering move to x:', x, 'and y:', y - 2)
-    if moves_on_board(x, y - 1):
-        print('Considering move to x:', x, 'and y:', y - 1)
+def find_pawn_moves(x, y, our_colour):
+    if our_colour == 'white':
+        if y == 6:
+            find_moves_in_direction(x, y, our_colour, 'north', 2)
+        else:
+            find_moves_in_direction(x, y, our_colour, 'north', 1)
+    if our_colour == 'black':
+        if y == 1:
+            find_moves_in_direction(x, y, our_colour, 'south', 2)
+        else:
+            find_moves_in_direction(x, y, our_colour, 'south', 1)    
     print()
 
-def find_bishop_moves(x, y):
-    for offset in range(1, 8):
-        move_x = x - offset
-        move_y = y - offset
-        if moves_on_board(move_x, move_y):
-            print('Considering North-Westward move to x:', move_x, 'and y:', move_y)
-    for offset in range(1, 8):
-        move_x = x + offset
-        move_y = y - offset
-        if moves_on_board(move_x, move_y):
-            print('Considering North-Eastward move to x:', move_x, 'and y:', move_y)
-    for offset in range(1, 8):
-        move_x = x + offset
-        move_y = y + offset
-        if moves_on_board(move_x, move_y):
-            print('Considering South-Eastward move to x:', move_x, 'and y:', move_y)
-    for offset in range(1, 8):
-        move_x = x - offset
-        move_y = y + offset
-        if moves_on_board(move_x, move_y):
-            print('Considering South-Westward move to x:', move_x, 'and y:', move_y)
+def find_bishop_moves(x, y, our_colour):
+    for i in ['north-east', 'south-east', 'south-west', 'north-west']:
+        find_moves_in_direction(x, y, our_colour, i, 8)
     print()
 
-    
-
-
-def find_rook_moves(x, y):
-    for xr in range(x + 1, 8):
-        print('Considering right move to x:', xr, 'and y:', y)
-        target = board[y][xr]
-    for xl in range(x - 1, -1, -1):
-        print('Considering left move to x:', xl, 'and y:', y)
-        target = board[y][xl]
-    for yd in range(y + 1, 8):
-        print('Considering downward move to x:', x, 'and y:', yd)
-        target = board[yd][x]
-    for yu in range(y - 1, -1, -1):
-        print('Considering upward move to x:', x, 'and y:', yu)
-        target = board[yu][x]
+def find_rook_moves(x, y, our_colour):
+    for i in ['north','east', 'south', 'west']:
+        find_moves_in_direction(x, y, our_colour, i, 8)
     print()
 
-def find_queen_moves(x, y):
+def find_queen_moves(x, y, our_colour):
     print('♕', 'Found Queen at x:', x, 'and y:', y)
-    find_bishop_moves(x, y)
-    find_rook_moves(x, y)
+    find_bishop_moves(x, y, our_colour)
+    find_rook_moves(x, y, our_colour)
+    print()
 
 def find_horsie_moves(x, y):
+    # TODO intergrate into direction moves function and add blocking and capture
     if moves_on_board(x - 1, y - 2):
         print('Considering two Up and one Left at x:', x - 1, 'and y:', y - 2)
     if moves_on_board(x + 1, y - 2):
@@ -144,6 +165,8 @@ def find_horsie_moves(x, y):
 
 # %%
 def find_all_moves(move_colour):
+    # TODO check all moves is valid
+    # TODO store moves
     for y in range(len(board)):
         for x in range(len(board[y])):
             piece = board[y][x]
@@ -151,19 +174,19 @@ def find_all_moves(move_colour):
             if move_colour == colour:
                 if piece == '♙' or piece == '♟':
                     print(piece, 'Found Pawn at x:', x, 'and y:', y)
-                    find_pawn_moves(x, y)
+                    find_pawn_moves(x, y, move_colour)
                 if piece == '♔' or piece == '♚':
                     print(piece, 'Found King at x:', x, 'and y:', y)
                     find_king_moves(x, y)
                 if piece == '♖' or piece == '♜':
                     print(piece, 'Found Rook at x:', x, 'and y:', y)
-                    find_rook_moves(x, y)
+                    find_rook_moves(x, y, move_colour)
                 if piece == '♗' or piece == '♝':
                     print(piece, 'Found Bishop at x:', x, 'and y:', y)
-                    find_bishop_moves(x, y)
+                    find_bishop_moves(x, y, move_colour)
                 if piece == '♕' or piece == '♛':
                     print(piece, 'Found Queen at x:', x, 'and y:', y)
-                    find_queen_moves(x, y)
+                    find_queen_moves(x, y, move_colour)
                 if piece == '♘' or piece == '♞':
                     print(piece, 'Found Horsie at x:', x, 'and y:', y)
                     find_horsie_moves(x, y)
@@ -172,6 +195,8 @@ find_all_moves('black')
 
 
 # %%
-
+#print(target_colour) 
+#print(our_colour)
+#print(target)
 
 
